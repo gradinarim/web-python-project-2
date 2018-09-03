257bdd175a858e353d55fd0ba7ceabacdd27529c
Ivan Gradinar <ivan.gradinar@netcracker.com>
2018-09-03 18:45:18 +0400

    Added secret key from config

--- application.py
+++ application.py
@@ -1,10 +1,15 @@
 import os
+import configparser
 
 from flask import Flask
 from flask_socketio import SocketIO, emit
 
+config = configparser.ConfigParser()
+config.read('config.cfg')
+SECRET_KEY = config['secret_key']['SECRET_KEY']
+
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SECRET_KEY"] = SECRET_KEY
 socketio = SocketIO(app)
 
 
